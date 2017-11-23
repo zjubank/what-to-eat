853373940815d688de3955d6fd5e42a3b480d2cd
zjubank <zjubank@gmail.com>
2017-11-24 01:51:40 +0800

    button select

--- prediction.php
+++ prediction.php
@@ -15,35 +15,61 @@
 <body>
 	<?php
 	  require 'navbar.php';
-
 	?>
 
-	<main role="main" class="container">
-    <h3>(*´▽｀)ノノ今天状态如何呢？</h3>
-    <div class="btn-group-vertical">
-      <button type="button" class="btn btn-outline-secondary" id="btnQ0A0">A.饿了~超想吃饭(*❦ω❦)</button>
-      <button type="button" class="btn btn-outline-secondary" id="btnQ0A1">B.有点饿，不是很想吃(╥╯^╰╥)</button>
-      <button type="button" class="btn btn-outline-secondary" id="btnQ0A2">C.不饿，但是好馋呀(¯﹃¯)</button>
-      <button type="button" class="btn btn-outline-secondary" id="btnQ0A3">D.不饿，也完全不想吃(灬°ω°灬)</button>
-    </div>
+	<main role="main" class="container" id="beforePredict">
+		<div class="question">
+	    <h3>(*´▽｀)ノノ今天状态如何呢？</h3>
+	    <div class="btn-group-vertical">
+	      <button type="button" class="btn btn-outline-secondary" id="btnQ0A0" selected=false>A.饿了~超想吃饭(*❦ω❦)</button>
+	      <button type="button" class="btn btn-outline-secondary" id="btnQ0A1" selected=false>B.有点饿，不是很想吃(╥╯^╰╥)</button>
+	      <button type="button" class="btn btn-outline-secondary" id="btnQ0A2" selected=false>C.不饿，但是好馋呀(¯﹃¯)</button>
+	      <button type="button" class="btn btn-outline-secondary" id="btnQ0A3" selected=false>D.不饿，也完全不想吃(灬°ω°灬)</button>
+	    </div>
+			<br><br>
+			<button type="button" class="btn btn-primary btn-prdct" onclick="btnPredict()">试试看</button>
+
+		</div>
 	</main>
 
   <script type ="text/javascript">
+			function btnPredict() {
+				var hasSelectedBtn = false;
+				for(var i = 0; i < 4; i++) {
+					var btnGrp = 0;
+					btnTestId="btnQ"+btnGrp+"A"+i;
+					if(document.getElementById(btnTestId).getAttribute("selected")=="true") {
+						predict(i);
+						hasSelectedBtn = true;
+					}
+				}
+				if(hasSelectedBtn == false) {
+					alert("请先选择一项~");
+				}
+			}
+
+			function predict(num){
+				// if(num==3) alert("您别吃了");
+
+			}
+			
       function btnSeletcted(btnGrp,btnNum){
         btnId="btnQ"+btnGrp+"A"+btnNum;
         for(var i=0; i<4; i++) {
           if(i==btnNum) {
             document.getElementById(btnId).classList.remove("btn-outline-secondary");
             document.getElementById(btnId).classList.add("btn-success");
+						document.getElementById(btnId).setAttribute("selected",true);
           }
           else {
             unSelectBtnId="btnQ"+btnGrp+"A"+i;
             document.getElementById(unSelectBtnId).classList.remove("btn-success");
             document.getElementById(unSelectBtnId).classList.add("btn-outline-secondary");
+						document.getElementById(unSelectBtnId).setAttribute("selected",false);
           }
         }
       }
-      
+
       document.getElementById("btnQ0A0").addEventListener("click",function() {
         btnSeletcted(0,0);
       });
